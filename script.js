a9d62b69e4a57bab6c6e9c495c09bf5319b7a1d0
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-06-07 17:45:35 -0700

    time is added so now the current date is presented at the top of the page. Still need to do the past/present/future feature and also get and set the local storage to save the information

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//TODO: entire js file LOL!
+//TODO: do the past/present/future things!
+$(document).ready(function() {
+    var currentDateTimeSpan = $("#currentDay");
+
+    //This function updates the current date to 
+    //display at the top of the page
+    function updateCurrentDate() {
+        currentDateTimeSpan.text(moment().format("dddd, MMMM Do YYYY"));
+    }
+    
+    updateCurrentDate();
+});
